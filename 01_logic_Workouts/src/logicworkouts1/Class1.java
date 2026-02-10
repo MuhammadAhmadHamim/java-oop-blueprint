44c87fbe9f8313b0e22655c3505f8fa2c4d8d4c5
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-10 21:25:57 +0500

    1st of many workouts..

---
+++ 01_logic_Workouts/src/logicworkouts1/Class1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logicworkouts1;
+
+/**
+ *
+ * @author cr7hi
+ */
+public class Class1 {
+    private String name;
+    private int age;
+    
+    void setName(String name){this.name=name;}
+    String getName(){return this.name;}
+    void setAge(int age){this.age=age;}
+    int getAge(){return this.age;}
+    
+    
+    public class Class2 {
+    private String name;
+    private int age;
+    }
+}
---
+++ 01_logic_Workouts/src/logicworkouts1/JavaApplication1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package logicworkouts1;
+
+/**
+ *
+ * @author cr7hi
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+        Class1 c1=new Class1();
+        Class1.Class2 c2;
+        c2=c1.new Class2();
+        System.out.println(c2);
+    }
+    
+}
